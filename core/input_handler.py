bb971dde793f0f189ce33d26b2e2fbaf1b540906
Samuel Melton <smelton@ethosgroup.com>
2023-03-29 11:09:33 -0500

    fixed input bug

--- core/input_handler.py
+++ core/input_handler.py
@@ -5,6 +5,7 @@ class InputHandler:
   def __init__(self) -> None:
     pg.joystick.init()
     joystick_count = pg.joystick.get_count()
+    self.joystick_initialized = False
     if joystick_count == 0:
       print("No joysticks detected.")
     else:
@@ -12,6 +13,7 @@ class InputHandler:
       self.joystick = pg.joystick.Joystick(0)
       self.joystick.init()
       print(f"joystick name: {self.joystick.get_name()}")
+      self.joystick_initialized = True
 
   def get_player_inputs(self):
     keys = pg.key.get_pressed()
@@ -22,16 +24,18 @@ class InputHandler:
       return_list.append('move_left') 
     if keys[pg.K_d]:
       return_list.append('move_right')
-    left_stick_axis = self.joystick.get_axis(0)
-    if left_stick_axis < - 0.3:
-      return_list.append('move_left')
-    if left_stick_axis > 0.3:
-      return_list.append('move_right')
+    if self.joystick_initialized:
+      left_stick_axis = self.joystick.get_axis(0)
+      if left_stick_axis < - 0.3:
+        return_list.append('move_left')
+      if left_stick_axis > 0.3:
+        return_list.append('move_right')
 
 		# jump
     if keys[pg.K_SPACE]:
       return_list.append('jump')
-    if self.joystick.get_button(0):
-      return_list.append('jump')
+    if self.joystick_initialized:
+      if self.joystick.get_button(0):
+        return_list.append('jump')
 
     return return_list
